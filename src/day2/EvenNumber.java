3393d25c7ea9246c2ddc460d6c287c4db1d752b7
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2022-06-21 21:20:00 +0530

    print even number

---
+++ src/day2/EvenNumber.java
@@ -0,0 +1,12 @@
+package day2;
+
+public class EvenNumber {
+    public static void main(String[] args) {
+        int a=50,i;
+        for(i=2;i<=a;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
