bb68cea0273702934fbc615c3c6eeed91f963c9f
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2022-06-21 21:31:08 +0530

    prime number

---
+++ src/day2/PrimeNumber.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class PrimeNumber {
+    public static boolean isPrime(int n){
+        for (int i = 2; i < n; i++) {
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        for (int i = 2; i <= 50; i++) {
+            if(isPrime(i)){
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
